525722452a092fd22bb1fbb94c7136bf503ff654
bhautik gohel <gohelbhautik1234@gmail.com>
2025-05-13 23:55:11 +0530

    update the structure folder

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
-require("./db/config")
+require("../db/config")
 const cors      = require('cors')
 const express   = require('express');
 const app       = express();
 app.use(express.json())
 app.use(cors());
-const userRoutes    = require('./routes/userroutes');
-const productRoutes = require('./routes/productRoutes');
+const userRoutes    = require('../routes/userroutes');
+const productRoutes = require('../routes/productRoutes');
 app.use('/',userRoutes);
 app.use('/',productRoutes);
 // app.listen(process.env.PORT || 5000);
