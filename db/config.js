ea4c4aa69d5295a310f8d9027641edfe0bd6e363
bhautik gohel <gohelbhautik1234@gmail.com>
2025-05-14 18:51:54 +0530

    connect with monogdb atlass connection use

--- db/config.js
+++ db/config.js
@@ -1,4 +1,7 @@
 require('dotenv').config();//load .env file
 const  mongoose = require("mongoose");
-mongoose.connect(process.env.MONGO_URI).then(()=>console.log('DB Connected')).catch((err)=>console.error('DB Error:', err));
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+}).then(()=>console.log('DB Connected')).catch((err)=>console.error('DB Error:', err));
 
