2823bfb913b75acc52ad597ec3653b88f31a6db1
bhautik gohel <gohelbhautik1234@gmail.com>
2025-05-15 23:23:08 +0530

    check uri mongodb

--- db/config.js
+++ db/config.js
@@ -1,4 +1,5 @@
 require('dotenv').config();//load .env file
 const  mongoose = require("mongoose");
+console.log("MONGO_URI = ",process.env.MONGO_URI)
 mongoose.connect(process.env.MONGO_URI).then(()=>console.log('DB Connected')).catch((err)=>console.error('DB Error:', err));
 
