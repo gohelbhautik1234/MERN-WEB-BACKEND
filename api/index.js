47515e0e7760eedea4604607a694f73535f6c266
bhautik gohel <gohelbhautik1234@gmail.com>
2025-05-16 10:51:42 +0530

    in mongodb there is not connect data permanant then change in data base connection code

--- api/index.js
+++ api/index.js
@@ -1,13 +1,18 @@
 require('dotenv').config();
-require("../db/config")
 const cors      = require('cors')
 const express   = require('express');
 const app       = express();
 app.use(express.json())
 app.use(cors());
+const port=process.env.PORT || 5000;
 const userRoutes    = require('../routes/userroutes');
 const productRoutes = require('../routes/productRoutes');
+const connectDB = require('../db/config');
 app.use('/',userRoutes);
 app.use('/',productRoutes);
-// app.listen(process.env.PORT || 5000);
+connectDB().then(()=>{
+    app.listen(port,()=>{
+            console.log(`Server running on port ${}`); 
+    });
+})
 module.exports=app;
--- db/config.js
+++ db/config.js
@@ -1,5 +1,16 @@
 require('dotenv').config();//load .env file
 const  mongoose = require("mongoose");
-console.log("MONGO_URI = ",process.env.MONGO_URI)
-mongoose.connect(process.env.MONGO_URI).then(()=>console.log('DB Connected')).catch((err)=>console.error('DB Error:', err));
 
+const connectDB=async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MonogDB Connected");
+    }
+    catch(err)
+    {
+        console.log("MongoDB Connection Failed", err.message);
+        process.exit(1);
+    }
+}
+
+module.exports=connectDB;
